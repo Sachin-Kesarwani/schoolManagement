47cb5a6790c6bf891b863152b0d4725cfebbbc28
Uzair <uzairmd073@gmail.com>
2023-06-06 23:51:17 +0530

    some work services

---
+++ my-school/src/Components/WhatWeOffer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../Styles/why.css'
+const Why = () => {
+  return (
+    <div className='why-cont'>
+       <div className='heading'>
+        <h1>
+         WHY US
+        </h1>
+        </div>
+        <div>
+            We follow three Step Process
+        </div>
+        <div className='main-box'>
+
+        <div>
+            <img src="https://www.drupal.org/files/project-images/reg_confirm_email_with_button_0.png" alt="" />
+        </div>
+        {/* <div>
+
+        <img src="https://www.nikahforever.com/arrow-red.jpg" alt="" />
+        </div> */}
+        <div>
+            <img src="https://previews.123rf.com/images/designsells/designsells2302/designsells230200554/198389317-online-test-education-violet-gradient-concept-with-people-scene-in-the-flat-cartoon-design-student.jpg" alt="" />
+        </div>
+        {/* <div>
+
+        <img src="https://www.nikahforever.com/arrow-red.jpg" alt="" />
+        </div> */}
+        <div>
+            <img src="https://img.freepik.com/free-vector/expert-approved-cartoon-character-holding-checkmark-symbol-hand-finished-task-done-sign-satisfactory-official-sanction-acceptance_335657-2369.jpg?w=2000" alt="" />
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Why
