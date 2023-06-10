9e7834f162edc5b369bb7e5696b26be3dc75d947
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-06-10 20:06:30 +0530

    some part done

--- backend/Middleware/AdminMiddleware.js
+++ backend/Middleware/AdminMiddleware.js
@@ -1,6 +1,19 @@
+const AdminModel = require("../Models/Admin")
 
 
 
-function adminmiddleware(req,res,next){
-    
+async function adminmiddleware(req,res,next){
+    let admindata=req.body
+    try {
+        let storeddata=await AdminModel.find({email:admindata.email})
+        if(storeddata){
+          res.status(200).send({msg:"This user is already exist"})
+        }else{
+         next()
+        }
+    } catch (error) {
+        res.status(400).send({msg:"Something went wrong"})
+    }
 }
+
+module.exports=adminmiddleware
