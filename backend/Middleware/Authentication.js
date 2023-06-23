cb1be912604b242bd982a31f87fa003e99f646ca
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-06-23 19:27:08 +0530

    some changes are made in students rout

--- backend/Middleware/Authentication.js
+++ backend/Middleware/Authentication.js
@@ -3,11 +3,12 @@ let jwt=require("jsonwebtoken")
 require("dotenv").config()
 let SignupModel=require("../Models/LogSignup");
 const AdminModel = require("../Models/Admin");
+const { StudentModel } = require("../Models/StudentModel");
 function Authentication(req,res,next){
 
  let token=req?.headers?.authorization?.split(" ")[1]
  jwt.verify(token, process.env.secretkey,async function(err, decoded) {
-   // console.log(decoded)
+ console.log(decoded)
   
    if(decoded){
     let storeduser=await SignupModel.findOne({_id:decoded.userid})
@@ -25,7 +26,20 @@ function Authentication(req,res,next){
    });
 }
 
+ async function  presentationOFstudent(req,res,next){
+  let {name}=req.body
+  try {
+    let storeddata=await StudentModel.find({name:name})
+   
+    if(storeddata.length>0){
+       res.status(200).send({msg:"Student Already Registered , Verfication is in Process"})
+    }else{
+      next()
+    }
+  } catch (error) {
+    res.status(400).send({msg:"Something went wrong"})
+  }
+ }
 
 
-
-module.exports={Authentication}
+module.exports={Authentication, presentationOFstudent}
--- backend/Routes/StudentRoute.js
+++ backend/Routes/StudentRoute.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const { StudentModel } = require("../Models/StudentModel")
-const { Authentication } = require("../Middleware/Authentication")
+const { Authentication, presentationOFstudent } = require("../Middleware/Authentication")
 const { AdminChecking } = require("../Middleware/AdminAuth")
 // const { Adminrouter } = require("../Middleware/AdminAuth")
 const StudentRouter=express.Router()
 
 
-StudentRouter.post("/register",Authentication,async(req,res)=>{
+StudentRouter.post("/register",Authentication,presentationOFstudent,async(req,res)=>{
      let data=req.body
     let fees={ january:false,
         february:false,
@@ -20,29 +20,78 @@ StudentRouter.post("/register",Authentication,async(req,res)=>{
         october:false,
         november:false,
         december:false,
-        delete_Admin:false}
-        data={...data,...fees,status:false}
+        delete_Admin:false,
+       
+        transport_driver: "",
+        transport_conductor: "",
+        class_teacher:"",
+        class_teacher_id: "",
+    }
+
+    if(data.transport){
+      let transport_fees={
+        transport_fees_january:false,
+        transport_fees_february:false,
+        transport_fees_march:false,
+        transport_fees_april:false,
+        transport_fees_may:false,
+        transport_fees_june:false,
+        transport_fees_july:false,
+        transport_fees_august:false,
+        transport_fees_september:false,
+        transport_fees_october:false,
+        transport_fees_november:false,
+        transport_fees_december:false,
+      }
+  
+        let distance=parseInt(Number(data.transport_to)/5)
+        transport_fees.transport_per_month_fees=400+(200*distance)
+     
+      data={...data,...fees,...transport_fees,status:false}
+    }else{
+      delete data.transport_from
+      delete data.transport_to
+      data={...data,...fees,status:false}
+    }
+       
     try {
+    
         let studentdata =new StudentModel(data)
+     
         await studentdata.save()
-        res.status(200).send({"msg":"Student Registered Successfully and Verification is pending by school"})
+     
+        res.status(200).send({"msg":"Registered Successfully and Verification is pending by school"})
     } catch (error) {
-        res.status(200).send({"msg":error.message})
+        res.status(400).send({"msg":"Someting went wrong"})
         
     }
 })
-StudentRouter.patch("update/:id",Authentication,async(req,res)=>{
+StudentRouter.patch("/update/:id",AdminChecking,async(req,res)=>{
     const {id} = req.params
      let data=req.body
+     console.log(data)
     try {
        await StudentModel.findByIdAndUpdate({_id:id},{data})
        
-        res.send({"msg":"user has been updated"})
+        res.status(200).send({"msg":"Student some data updated"})
     } catch (error) {
-        res.send({"msg":error.message})
+        res.status(400).send({"msg":"Something Went Wrong"})
         
     }
 })
+
+
+StudentRouter.get("/allStudents/:userid",Authentication,async(req,res)=>{
+    let {userid}=req.params
+      try {
+        let allstudent=await StudentModel.find({userid})
+        res.status(200).send({"msg":"Your Childrens",data:allstudent})
+      } catch (error) {
+        res.status(400).send({msg:"Something went wrong"})
+      }
+})
+
+
 
 StudentRouter.get("/allStudents",AdminChecking,async(req,res)=>{
     try {
